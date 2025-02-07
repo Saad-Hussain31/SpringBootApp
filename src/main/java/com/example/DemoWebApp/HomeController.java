3d29d70da18f6895a50b87df465aef7ff3548fe8
Saad Hussain <saadnasir31@gmail.com>
2025-02-07 17:23:06 +0100

    Minimal Rest Controller

---
+++ src/main/java/com/example/DemoWebApp/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.DemoWebApp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String greet() {
+        return "Welcome to the Homepage!";
+    }
+
+    @RequestMapping("/about")
+    public String about() {
+        return "This is the about page!";
+    }
+}
