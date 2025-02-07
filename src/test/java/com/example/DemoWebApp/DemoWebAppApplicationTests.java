095f92407646c974aed95501a718452ce33ab0cf
Saad Hussain <saadnasir31@gmail.com>
2025-02-07 17:15:32 +0100

    getting started

---
+++ src/test/java/com/example/DemoWebApp/DemoWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DemoWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
