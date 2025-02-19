e785be37dfeae812aebefac6b058395e54f3b6f4
Saad Hussain <saadnasir31@gmail.com>
2025-02-19 16:37:25 +0100

    ORM using hibernate and H2 databse

--- src/main/java/com/example/DemoWebApp/model/Product.java
+++ src/main/java/com/example/DemoWebApp/model/Product.java
@@ -1,15 +1,20 @@
 package com.example.DemoWebApp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 
-
+@Component
+@Entity
 public class Product {
+    @Id
     private int prodId;
     private String prodName;
     private int price;
 
-    public Product(int prodId, String prodName, int price) {
+    public Product() {
         this.prodId = prodId;
         this.prodName = prodName;
         this.price = price;
--- src/main/java/com/example/DemoWebApp/service/ProductService.java
+++ src/main/java/com/example/DemoWebApp/service/ProductService.java
@@ -1,6 +1,8 @@
 package com.example.DemoWebApp.service;
 
 import com.example.DemoWebApp.model.Product;
+import com.example.DemoWebApp.repo.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.sql.Array;
@@ -11,48 +13,26 @@ import java.util.List;
 @Service
 public class ProductService {
 
-    List<Product> products = new ArrayList<>(Arrays.asList(new Product(101,
-            "iPhone", 50000),
-            new Product(102,"Canon", 70000)));
+    @Autowired
+    ProductRepo repo;
+
     public List<Product> getProducts() {
-        return products;
+        return repo.findAll();
     }
 
     public Product getProductById(int prodId) {
-        return products.stream()
-                .filter(p -> p.getProdId() == prodId)
-                .findFirst()
-                .orElse(new Product (100,"No Item", 0));
+        return repo.findById(prodId).orElse(new Product());
     }
 
-//    public Product getProductById(int prodId, boolean returnDefault) { // Overloaded method
-//        return products.stream()
-//                .filter(p -> p.getProdId() == prodId)
-//                .findFirst()
-//                .orElse(returnDefault ? new Product(100, "No Item", 0) : null);
-//    }
-
     public void addProduct(Product prod) {
-        products.add(prod);
+        repo.save(prod);
     }
 
     public void updateProduct(Product prod) {
-        int index =0;
-        for(int i=0; i<products.size(); i++) {
-            if(prod.getProdId() == products.get(i).getProdId()) {
-                index = i;
-            }
-        }
-        products.set(index, prod);
+        repo.save(prod);
     }
 
     public void deleteProduct(int prodId) {
-        int index =0;
-        for(int i=0; i<products.size(); i++) {
-            if(prodId == products.get(i).getProdId()) {
-                index = i;
-            }
-        }
-        products.remove(index);
+        repo.deleteById(prodId);
     }
 }
